2a24c2fcb85450e25eb73a8dbbeb837c2c800a87
Vladimir <always-optimus@yandex.ru>
2018-10-06 22:25:07 +0300

    Это наш первый коммит

---
+++ script.js
@@ -0,0 +1,12 @@
+var btn1 = document.getElementById("btn1");
+var btn2 = document.getElementById("btn2");
+var main = document.getElementById("main");
+var myList = document.getElementById("list");
+var ul = document.getElementsByTagName("ul");
+
+
+
+
+console.log(btn1);
+console.log(btn2);
+console.log(ul[0]);
